fb2a4baf1e7dc11fb5a657d3ba8cdcfcca011558
Henry Zimmerman <zimhen7@gmail.com>
2017-10-27 17:33:54 -0400

    reorganized db code

--- src/main.rs
+++ src/main.rs
@@ -28,15 +28,7 @@ use uuid::Uuid;
 
 mod routes;
 use routes::*;
-
-
-use diesel::sqlite::SqliteConnection;
-use r2d2_diesel::ConnectionManager;
-
-use std::ops::Deref;
-use rocket::http::Status;
-use rocket::request::{self, FromRequest};
-use rocket::{Request, State, Outcome};
+mod db;
 
 
 use simplelog::{Config, TermLogger, WriteLogger, CombinedLogger, LogLevelFilter};
@@ -68,47 +60,6 @@ pub fn init_rocket() -> Rocket {
 
 
 
-// An alias to the type for a pool of Diesel SQLite connections.
-type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
-
-// The URL to the database, set via the `DATABASE_URL` environment variable.
-static DATABASE_URL: &'static str = env!("DATABASE_URL");
-
-/// Initializes a database pool.
-fn init_pool() -> Pool {
-    let config = r2d2::Config::default();
-    let manager = ConnectionManager::<SqliteConnection>::new(DATABASE_URL);
-    r2d2::Pool::new(config, manager).expect("db pool")
-}
-
-
-
-// Connection request guard type: a wrapper around an r2d2 pooled connection.
-pub struct DbConn(pub r2d2::PooledConnection<ConnectionManager<SqliteConnection>>);
-
-/// Attempts to retrieve a single connection from the managed database pool. If
-/// no pool is currently managed, fails with an `InternalServerError` status. If
-/// no connections are available, fails with a `ServiceUnavailable` status.
-impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
-    type Error = ();
-
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<DbConn, ()> {
-        let pool = request.guard::<State<Pool>>()?;
-        match pool.get() {
-            Ok(conn) => Outcome::Success(DbConn(conn)),
-            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
-        }
-    }
-}
-
-// For the convenience of using an &DbConn as an &SqliteConnection.
-impl Deref for DbConn {
-    type Target = SqliteConnection;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
 
 ///Path should be an &str that starts with a /
 fn format_api(path: &str) -> String {
