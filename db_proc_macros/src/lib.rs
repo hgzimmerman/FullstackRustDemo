9853485c6741389ab59ff874a28fd9db018aca89
Henry Zimmerman <zimhen7@gmail.com>
2018-03-02 18:41:31 -0600

    Implement CRD macro and implement it on answers

---
+++ db_proc_macros/src/lib.rs
@@ -0,0 +1,107 @@
+#![feature(proc_macro)]
+#![recursion_limit="128"]
+extern crate proc_macro;
+extern crate syn;
+#[macro_use]
+extern crate quote;
+
+use proc_macro::TokenStream;
+
+
+/// CRD stands for Create, Read, Delete.
+/// This macro takes a given type and derives methods that allow it to be
+/// inserted, read from, and deleted from a database.
+#[proc_macro_derive(Crd, attributes(table_name, insertable))]
+pub fn crd(input: TokenStream) -> TokenStream {
+    // Parse the string representation
+    let ast = syn::parse(input).unwrap();
+
+    // Build the impl
+    let gen = impl_crd(&ast);
+    // Return the generated impl
+    gen.into()
+}
+
+fn impl_crd( ast: &syn::DeriveInput) -> quote::Tokens {
+    let name: &syn::Ident = &ast.ident;
+
+    let table_name: syn::Ident = get_value_from_attributes("table_name", &ast.attrs);
+    let insertable: syn::Ident = get_value_from_attributes("insertable", &ast.attrs);
+
+    quote! {
+        impl Creatable<#insertable> for #name {
+            fn create(insert: #insertable, conn: &Conn) -> Result<Self, WeekendAtJoesError> {
+                use schema::#table_name;
+
+                diesel::insert_into(#table_name ::table)
+                    .values(&insert)
+                    .get_result(conn.deref())
+                    .map_err(#name::handle_error)
+            }
+
+        }
+
+        impl<'a> Retrievable<'a> for #name {
+            fn get_by_id(item_id: i32, conn: &Conn) -> Result<#name, WeekendAtJoesError> {
+                use schema::answers::dsl::*;
+
+                #table_name
+                    .find(item_id)
+                    .first::<#name>(conn.deref())
+                    .map_err(Answer::handle_error)
+            }
+        }
+
+        impl<'a> Deletable<'a> for #name {
+            fn delete_by_id(item_id: i32, conn: &Conn) -> Result<#name, WeekendAtJoesError> {
+                use schema::answers::dsl::*;
+
+                let target = #table_name.filter(id.eq(item_id));
+
+                diesel::delete(target)
+                    .get_result(conn.deref())
+                    .map_err(#name::handle_error)
+            }
+        }
+
+
+        impl<'a> CRD<'a, #insertable> for #name {}
+    }
+}
+
+
+/// Given a string that coreesponds to the ident specified in the attributes section in the proc_macro_derive(...) above
+/// extract the value that corresponds to it.
+fn get_value_from_attributes(attribute_name: &'static str, attrs: &Vec<syn::Attribute>) -> syn::Ident {
+    attrs
+        .into_iter()
+        .map(|x| x.interpret_meta(). unwrap())
+        .filter(|x| {
+
+            if let syn::Meta::NameValue(ref t) = *x {
+                // We found our attribute, now extract the value in the next map.
+                if t.ident.as_ref() == attribute_name {
+                    return true
+                }
+                else {
+                    false
+                }
+            } else {
+                false // the attribute wasn't in the proper form: "thing" = value
+            }
+        })
+        .map(|x| {
+            if let syn::Meta::NameValue(t) = x {
+                if let syn::Lit::Str(l) = t.lit {
+                    let ident = syn::Ident::from(l.value());
+                    return ident
+                }
+                panic!("Couldn't extract value from attribute")
+            }
+            panic!("Wasn't namevalue")
+        })
+        .collect::<Vec<syn::Ident>>()
+        .first()
+        .expect(format!("The attribute '{}' could not be found", attribute_name).as_ref())
+        .clone()
+}
--- src/db/answer.rs
+++ src/db/answer.rs
@@ -16,10 +16,14 @@ use diesel::ExpressionMethods;
 
 
 
-#[derive(Debug, Clone, Identifiable, Queryable, Associations)]
+#[derive(Debug, Clone, Identifiable, Queryable, Associations, Crd)]
 #[table_name = "answers"]
+#[insertable = "NewAnswer"]
 #[belongs_to(User, foreign_key = "author_id")]
 #[belongs_to(Question, foreign_key = "question_id")]
+// #[insertable = "NewAnswer"]
+// #[TableName = "answers"]
+// #[InsertedType = "NewAnswer"]
 pub struct Answer {
     /// Primary Key.
     pub id: i32,
@@ -41,43 +45,42 @@ pub struct AnswerData {
     pub user: User,
 }
 
-impl Creatable<NewAnswer> for Answer {
-    /// Creates a new answer
-    fn create(new_answer: NewAnswer, conn: &Conn) -> Result<Answer, WeekendAtJoesError> {
-        use schema::answers;
+// impl Creatable<NewAnswer> for Answer {
+//     /// Creates a new answer
+//     fn create(new_answer: NewAnswer, conn: &Conn) -> Result<Answer, WeekendAtJoesError> {
+//         use schema::answers;
 
-        diesel::insert_into(answers::table)
-            .values(&new_answer)
-            .get_result(conn.deref())
-            .map_err(Answer::handle_error)
-    }
-}
+//         diesel::insert_into(answers::table)
+//             .values(&new_answer)
+//             .get_result(conn.deref())
+//             .map_err(Answer::handle_error)
+//     }
+// }
 
-impl<'a> Retrievable<'a> for Answer {
-    fn get_by_id(answer_id: i32, conn: &Conn) -> Result<Answer, WeekendAtJoesError> {
-        use schema::answers::dsl::*;
+// impl<'a> Retrievable<'a> for Answer {
+//     fn get_by_id(answer_id: i32, conn: &Conn) -> Result<Answer, WeekendAtJoesError> {
+//         use schema::answers::dsl::*;
 
-        // Gets the first answer that matches the id.
-        answers
-            .find(answer_id)
-            .first::<Answer>(conn.deref())
-            .map_err(Answer::handle_error)
-    }
-}
+//         // Gets the first answer that matches the id.
+//         answers
+//             .find(answer_id)
+//             .first::<Answer>(conn.deref())
+//             .map_err(Answer::handle_error)
+//     }
+// }
 
-impl<'a> Deletable<'a> for Answer {
-    fn delete_by_id(answer_id: i32, conn: &Conn) -> Result<Answer, WeekendAtJoesError> {
-        use schema::answers::dsl::*;
+// impl<'a> Deletable<'a> for Answer {
+//     fn delete_by_id(answer_id: i32, conn: &Conn) -> Result<Answer, WeekendAtJoesError> {
+//         use schema::answers::dsl::*;
 
-        let target = answers.filter(id.eq(answer_id));
+//         let target = answers.filter(id.eq(answer_id));
 
-        diesel::delete(target)
-            .get_result(conn.deref())
-            .map_err(Answer::handle_error)
-    }
-}
+//         diesel::delete(target)
+//             .get_result(conn.deref())
+//             .map_err(Answer::handle_error)
+//     }
+// }
 
-impl<'a> CRD<'a, NewAnswer> for Answer {}
 
 impl ErrorFormatter for Answer {
     fn handle_error(diesel_error: Error) -> WeekendAtJoesError {
