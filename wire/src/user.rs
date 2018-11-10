40d17c279fd9b05a7dd73ba8e48b1cebf4e70780
Henry Zimmerman <zimhen7@gmail.com>
2018-11-10 12:12:56 -0500

    missed file

--- wire/src/user.rs
+++ wire/src/user.rs
@@ -66,7 +66,6 @@ impl Default for Jwt {
 
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub enum UserRole {
-    Unprivileged,
     Moderator,
     Admin,
     Publisher,
@@ -75,7 +74,6 @@ pub enum UserRole {
 impl From<UserRole> for i32 {
     fn from(role: UserRole) -> i32 {
         match role {
-            UserRole::Unprivileged => 1,
             UserRole::Moderator => 2,
             UserRole::Admin => 3,
             UserRole::Publisher => 4,
@@ -86,13 +84,11 @@ impl From<UserRole> for i32 {
 impl From<i32> for UserRole {
     fn from(number: i32) -> UserRole {
         match number {
-            1 => UserRole::Unprivileged,
             2 => UserRole::Moderator,
             3 => UserRole::Admin,
             4 => UserRole::Publisher,
             _ => {
-                eprintln!("Tried to convert an unsupported number into a user role");
-                UserRole::Unprivileged
+                panic!("Tried to convert an unsupported number into a user role");
             }
         }
     }
